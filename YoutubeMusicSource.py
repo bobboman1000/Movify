c553a38b2cef03b53eff35ba47826d47f98c8418
Benjamin Jochum <uzebb@student.kit.edu>
2021-12-03 15:41:33 +0100

    Add updated yt source

--- YoutubeMusicSource.py
+++ YoutubeMusicSource.py
@@ -2,7 +2,7 @@ import sys
 
 import pandas as pd
 from ytmusicapi import YTMusic
-from MusicServicSource import LibraryObject, Playlist
+from MusicServicSource import LibraryObject
 
 
 class YoutubeMusicSource:
@@ -16,13 +16,11 @@ class YoutubeMusicSource:
             print("Cannot establish connection. Please check the integrity of your auth json.")
             sys.exit(1)
 
-
     def fetch_albums_library(self):
         albums_response = self.ytmusic.get_library_albums(limit=10000)
         parse_album = lambda album: LibraryObject(album["title"], [artist["name"] for artist in album["artists"]], album["year"], album["type"])
         self.albums = [parse_album(album) for album in albums_response]
-        self.library = self.get_df()
-
+        return self.albums
 
     def get_df(self):
         if self.albums:
